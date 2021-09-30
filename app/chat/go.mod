60aa1c757e9c2dc229deef819e0785cdd94a1bd6
S. Nishizawa <s.nishizawa@Mac-mini-3.local>
2021-09-30 19:09:03 +0900

    add app

---
+++ app/chat/go.mod
@@ -0,0 +1,26 @@
+module chat
+
+go 1.17
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/gomniauth v0.0.0-20170717123514-4b6c822be2eb
+	github.com/stretchr/objx v0.3.0
+	github.com/za-wave/goblueprints v0.0.0-20210923012029-6523135605a5
+)
+
+require (
+	github.com/clbanning/x2j v0.0.0-20191024224557-825249438eec // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
+	github.com/stretchr/signature v0.0.0-20160104132143-168b2a1e1b56 // indirect
+	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
---
+++ app/chat/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+	"github.com/za-wave/goblueprints/chapter1/trace"
+)
+
+// set the active Avatar implementation
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar,
+}
+
+// templ represents a single template
+type templateHandler struct {
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if t.templ == nil {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	}
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
+}
+
+var host = flag.String("host", ":8080", "The host of the application.")
+
+func main() {
+	err := godotenv.Load(fmt.Sprintf("../../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+	AuthSecurityKey := os.Getenv("AUTH_SECURITY_KEY")
+	GoogleClientID := os.Getenv("GOOGLE_CLIENT_ID")
+	GoogleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+
+	flag.Parse() // parse the flags
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey(AuthSecurityKey)
+	gomniauth.WithProviders(
+		// facebook.New("", "", "http://localhost:8080/auth/callback/facebook"),
+		// github.New("", "", "http://localhost:8080/auth/callback/github"),
+		google.New(GoogleClientID, GoogleClientSecret, "http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/",
+			http.FileServer(http.Dir("./avatars"))))
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *host)
+	if err := http.ListenAndServe(*host, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
+}
